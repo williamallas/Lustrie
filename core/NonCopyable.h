d044b75df0f1c31800b73a7c99eabfd0cfdd26f6
Williamallas <williamallas@aol.com>
2017-02-25 22:56:38 +0100

    NonCopyable

---
+++ core/NonCopyable.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class NonCopyable
+{
+public:
+	NonCopyable() = default;
+
+private:
+	NonCopyable(const NonCopyable&) = delete;
+	NonCopyable(NonCopyable&&) = delete;
+
+	NonCopyable& operator=(const NonCopyable&) = delete;
+	NonCopyable& operator=(NonCopyable&&) = delete;
+};
